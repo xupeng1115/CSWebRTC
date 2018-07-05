bec3469e5834730f1589da403e63e447046eb225
markxu <15136723391@sina.cn>
2018-07-05 18:15:24 +0800

    添加视频面试完整流程

---
+++ Scripts/js/Index.js
@@ -0,0 +1,16 @@
+(function(time){
+
+}(window.countDown={}));
+
+$(function(){
+    $('#video-box').niceScroll({
+        cursorcolor: "#000",
+        cursoropacitymax: 0.2, 
+        touchbehavior:false, 
+        cursorwidth: "5px", 
+        cursorborder: "0", 
+        cursorborderradius: "5px",
+        autohidemode: true,
+    });
+});
+
